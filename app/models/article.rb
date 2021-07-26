ce0fe8c59a4aca73f8986603af885d11aa809920
Max <madmacj@gmail.com>
2021-07-26 23:37:24 +0300

    fix model aRTICLE SEARCH + fix tags view element

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,13 +2,13 @@ require 'elasticsearch/model'
 
 class Article < ActiveRecord::Base  
   extend ActiveSupport::Concern
-  # include Elasticsearch::Model
-  # include Elasticsearch::Model::Callbacks
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
-  # field :title, type: String
-  # field :body, type: String
-  # field :tags, type: String
-  #index_name "articles-#{Rails.env}"
+  field :title, type: String
+  field :body, type: String
+  field :tags, type: String
+  index_name "articles-#{Rails.env}"
 
   scope :with_category, ->(id) { where(cathegory_id: id) }
   serialize :tags, Array
@@ -35,28 +35,28 @@ class Article < ActiveRecord::Base
     words.join(" ") + "..."
   end
 
-  # # elasticsearch
-  # after_commit on: [:create] do
-  #   begin
-  #     __elasticsearch__.index_document
-  #   rescue Exception => ex
-  #     logger.error "ElasticSearch after_commit error on create: #{ex.message}"
-  #   end
-  # end
+  # elasticsearch
+  after_commit on: [:create] do
+    begin
+      __elasticsearch__.index_document
+    rescue Exception => ex
+      logger.error "ElasticSearch after_commit error on create: #{ex.message}"
+    end
+  end
 
-  # after_commit on: [:update] do
-  #   begin
-  #     Elasticsearch::Model.client.exists?(index: 'articles', type: 'article', id: self.id) ? __elasticsearch__.update_document :     __elasticsearch__.index_document
-  #   rescue Exception => ex
-  #     logger.error "ElasticSearch after_commit error on update: #{ex.message}"
-  #   end
-  # end
+  after_commit on: [:update] do
+    begin
+      Elasticsearch::Model.client.exists?(index: 'articles', type: 'article', id: self.id) ? __elasticsearch__.update_document :     __elasticsearch__.index_document
+    rescue Exception => ex
+      logger.error "ElasticSearch after_commit error on update: #{ex.message}"
+    end
+  end
 
-  # after_commit on: [:destroy] do
-  #   begin
-  #     __elasticsearch__.delete_document
-  #   rescue Exception => ex
-  #     logger.error "ElasticSearch after_commit error on delete: #{ex.message}"
-  #   end
-  # end
+  after_commit on: [:destroy] do
+    begin
+      __elasticsearch__.delete_document
+    rescue Exception => ex
+      logger.error "ElasticSearch after_commit error on delete: #{ex.message}"
+    end
+  end
 end
